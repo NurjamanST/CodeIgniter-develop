4a649536cb82d934668d49fbf34ef1951af163d7
NurjamanST <nurjamanst946@gmail.com>
2025-05-15 00:53:40 +0700

    Update DB Hosting

--- application/config/database.php
+++ application/config/database.php
@@ -70,10 +70,14 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 $active_group = 'default';
 $db['default'] = array(
 	'dsn'	=> '',
+	// 'hostname' => 'localhost',
+	// 'username' => 'root',
+	// 'password' => '',
+	// 'database' => 'db_haweshop',
 	'hostname' => 'localhost',
-	'username' => 'root',
-	'password' => '',
-	'database' => 'db_haweshop',
+	'username' => 'u683701725_scriptechlight',
+	'password' => 'CahayaTech24#$*',
+	'database' => 'u683701725_db_haweshop',
 	'dbdriver' => 'mysqli',
 	'dbprefix' => '',
 	'pconnect' => FALSE,
